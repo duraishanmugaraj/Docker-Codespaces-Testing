38a57f66b1c0f6e4abd9cd419c0d23e058f607af
Durai <duraishanmugaraj.r@gmail.com>
2024-08-18 03:12:43 +0530

    Docker local setup ✔✔

--- start_services.sh
+++ start_services.sh
@@ -3,12 +3,12 @@
 /opt/spark/bin/spark-class org.apache.spark.deploy.master.Master &
 
 # Set Jupyter Notebook configuration if it doesn't exist
-if [ ! -f /`whoami`/.jupyter/jupyter_notebook_config.py ]; then
+if [ ! -f /$(whoami)/.jupyter/jupyter_notebook_config.py ]; then
     jupyter notebook --generate-config -y
 fi
 
-echo "c.NotebookApp.password = ''" >> /`whoami`/.jupyter/jupyter_notebook_config.py
-echo "c.NotebookApp.token = ''" >> /`whoami`/.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.password = ''" >> /$(whoami)/.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.token = ''" >> /$(whoami)/.jupyter/jupyter_notebook_config.py
 # Start Jupyter Notebook
 jupyter notebook --ip='*' --port=8888 --no-browser --allow-root &
 
